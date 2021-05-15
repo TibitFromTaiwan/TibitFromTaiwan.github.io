75bbca3ee1a261c5af7ca9132e60a41f94efd5e2
Li Chun Hua <jim870718@gmail.com>
2021-05-15 23:24:13 +0800

    .

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
+var menuIsClicked = false
+var menu = document.querySelector('.menu')
+var menuIconTop = document.querySelector('.menu-button-icon .top-side')
+var menuIconBottom = document.querySelector('.menu-button-icon .bottom-side')
+
 document.querySelector('.menu-button').addEventListener('click', () => {
-    var menu = document.querySelector('.menu')
-    if (menu.style.backdropFilter) {
+    menuIsClicked = !menuIsClicked
+    if (menuIsClicked) {
+        menu.style.backdropFilter = 'blur(15px)'
+        menu.style.backgroundColor = 'hsla(0, 0%, 100%, 0.1)'
+        menuIconTop.style.transform = 'translateX(3px) rotate(45deg)'
+        menuIconBottom.style.transform = 'translateX(3px) rotate(-45deg)'
+    } else {
         menu.style.backdropFilter = ''
         menu.style.backgroundColor = 'hsla(0, 0%, 100%, 0)'
-    } else {
-        menu.style.backdropFilter = 'blur(10px)'
-        menu.style.backgroundColor = 'hsla(0, 0%, 100%, 0.)'
+        menuIconTop.style.transform = ''
+        menuIconBottom.style.transform = ''
     }
 })
